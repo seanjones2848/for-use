56aedafb1419a6f3b84b00629460052a0988d39e
sean jones <seanjones2848@gmail.com>
2017-04-08 16:56:40 -0700

    better?

--- mares/catcher.sh
+++ mares/catcher.sh
@@ -1,7 +1,8 @@
 ./mangercentos & PID=$!
 kill -14 $PID
-read -n 1 -p "Dearest King, The horses have been coralled, would you like them tamedor to remain wild? (t/w)" ans
-if &ans = "t"; then
+echo "Dearest King, The horses have been coralled, would you like them tamedor to remain wild? (t/w)"
+read ans
+if $(ans) ="t"; then
 	./tame & echo "Here are your tamed horses."
 else
 	./wild & echo "You asked for it..."
