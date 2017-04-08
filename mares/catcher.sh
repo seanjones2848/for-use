75a794ff3ed444032a078d02693032b65e49e992
sean jones <seanjones2848@gmail.com>
2017-04-08 16:34:03 -0700

    with some choice

--- mares/catcher.sh
+++ mares/catcher.sh
@@ -1,4 +1,8 @@
 ./mangercentos & PID=$!
-echo $PID
 kill -14 $PID
-echo "Horses coralled"
+read -n 1 -p "Dearest King, The horses have been coralled, would you like them tamedor to remain wild? (t/w)" ans
+if echo "$ans" | grep -iq "^t"
+	./tame & echo "Here are your tamed horses."
+else
+	./wild & echo "You asked for it..."
+fi
