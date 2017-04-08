889e68562a1a7523d4f9a790b2d20c75b56317b4
sean jones <seanjones2848@gmail.com>
2017-04-08 16:38:06 -0700

    more

--- mares/catcher.sh
+++ mares/catcher.sh
@@ -1,7 +1,7 @@
 ./mangercentos & PID=$!
 kill -14 $PID
 read -n 1 -p "Dearest King, The horses have been coralled, would you like them tamedor to remain wild? (t/w)" ans
-if echo "$ans" | grep -iq "^t"
+if echo "$ans" | grep -iq "^t" ;then
 	./tame & echo "Here are your tamed horses."
 else
 	./wild & echo "You asked for it..."
