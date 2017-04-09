5c034b34c936cd5828eab13e9e2d38ebececb46c
sean jones <seanjones2848@gmail.com>
2017-04-08 17:04:07 -0700

    derp

--- mares/catcher.sh
+++ mares/catcher.sh
@@ -2,8 +2,7 @@
 kill -14 $PID
 echo "Dearest King, The horses have been coralled, would you like them tamedor to remain wild? (t/w)"
 read ans
-if $(ans) ="t"; then
-	./tame & echo "Here are your tamed horses."
-else
-	./wild & echo "You asked for it..."
+if ([$ans ='t'])
+then ./tame & echo "Here are your tamed horses."
+else ./wild & echo "You asked for it..."
 fi
