21e9c6e06ca28aeacffcf60a3a8e5cca53efe3c4
sean jones <seanjones2848@gmail.com>
2017-04-08 21:17:35 -0700

    fixed syntax in bash cases

--- mares/catcher.sh
+++ mares/catcher.sh
@@ -3,7 +3,7 @@ kill -14 $PID
 echo "Dearest King, The horses have been coralled, would you like them tamedor to remain wild? (t/w)"
 read ans
 case $ans
-	't') ./wild 0 & echo "Here are your tamed horses."; ;;
-	'w') ./wild 1 & echo "You asked for it..."; ;;
-	'*') echo "You failed to pick an option"; ;;
+	't') ./wild 0 & echo "Here are your tamed horses." ;;
+	'w') ./wild 1 & echo "You asked for it..." ;;
+	'*') echo "You failed to pick an option" ;;
 esac
